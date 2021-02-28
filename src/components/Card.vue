db78ac3d1fb98230bb854f5ef0d81be6f00a5144
TawhidSajib <tauhidulsajib34@gmail.com>
2021-03-01 02:27:45 +0600

    some change

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,20 +1,19 @@
 <template>
-<ol>
-    <li v-for="student in students" :key="student">Name: {{ student.name }} - Age: {{ student.age }}</li>
-</ol>
+  <div>
+    <ol>
+        <li v-for="p in persons">{{ p }}</li>
+    </ol>
+  </div>
+
+
 </template>
 <script>
 export default {
     data(){
         return{
-            students: [
-                {name: 'sajib',age: 23},
-                {name: 'Rafiq',age: 26},
-                {name: 'Karim',age: 25}
-            
-            
-            ]
+           persons: ['sajib','karim','rafiq']
         }
-    }
+    },
+   
 }
 </script>
